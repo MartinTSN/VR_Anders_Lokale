063684c84c65afcf1755fd088bafe73f7bbb5e32
Martin Thue Schilling Nissen <mart038e@edu.campusvejle.dk>
2018-12-10 09:50:23 +0100

    everything

---
+++ Assets/Scripts/Check_if_Watched.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR;
+using Valve.VR.InteractionSystem;
+
+public class Check_if_Watched : MonoBehaviour
+{
+    public Canvas Chat;
+
+    void Update()
+    {
+        Ray raycast = new Ray(transform.position, transform.forward);
+        Debug.DrawRay(raycast.origin, raycast.direction * 100);
+        RaycastHit hit;
+
+        if (Physics.Raycast(raycast, out hit))
+        {
+            if (hit.collider.gameObject.name == "Chat")
+            {
+                if (gameObject.GetComponentInParent<Hand>().gameObject.transform.rotation.y > 70)
+                {
+                    for (int i = 0; i < Chat.transform.childCount; i++)
+                    {
+                        Chat.transform.GetChild(i).gameObject.SetActive(true);
+                    }
+                }
+            }
+            else
+            {
+                for (int i = 0; i < Chat.transform.childCount; i++)
+                {
+                    Chat.transform.GetChild(i).gameObject.SetActive(false);
+                }
+            }
+        }
+    }
+}
